20f97ffb4fe4c32cf435d4c36ce26e20838f7ccf
anirudhlakkaraju <lakanirudh123@gmail.com>
2026-04-26 16:38:05 -0500

    Initial Commit

---
+++ Redpoint/RedpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedpointApp.swift
+//  Redpoint
+//
+//  Created by Anirudh Lakkaraju on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RedpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
